0c4d6128b8876d3c35bb8bbe4bdab2bbaf236e45
Darold Gilles <gilles@darold.net>
2013-08-07 00:24:11 +0200

    Fix some errors in cron lines. Thanks to Dominique Fournier for the patch.

--- debian/sendmailanalyzer.cron.d
+++ debian/sendmailanalyzer.cron.d
@@ -1,6 +1,6 @@
 # SendmailAnalyzer log reporting daily cache
-0 1 * * * /usr/bin/sa_cache > /dev/null 2>&1
+0 1 * * * root /usr/bin/sa_cache > /dev/null 2>&1
 # Daemon restart after mail.log logrotate (cron jobs at 6:25 every day)
-26 6 * * * /etc/init.d/sendmailanalyzer restart >/dev/null 2>&1
+26 6 * * * root /etc/init.d/sendmailanalyzer restart >/dev/null 2>&1
 # On huge MTA you may want to have five minutes caching
-*/5 * * * * /usr/bin/sa_cache -a > /dev/null 2>&1
+*/5 * * * * root /usr/bin/sa_cache -a > /dev/null 2>&1
